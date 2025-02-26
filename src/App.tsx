be23a41cfec89eedb0c6b2034facbe24a2c7d003
Om-k <om.k.poojary@gmail.com>
2025-02-26 23:44:18 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-primary' >
+     <h1>Test applcation</h1>
+     <p className='mt-5' >Lermw dasoidoas ioasjd ioajs ij d i ai  aslnlksanflkn asl lknkflsan klasnkldnaslk n</p>
+    </div>
+  )
+}
+
+export default App
