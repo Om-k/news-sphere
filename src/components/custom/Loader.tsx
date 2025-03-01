e3f9e3b919e37806a078cb229d8b937776f70f65
Om-k <om.k.poojary@gmail.com>
2025-03-01 20:44:20 +0530

    Update: to loader

--- src/components/custom/Loader.tsx
+++ src/components/custom/Loader.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Loader: React.FC = () => {
   return (
-    <div className="flex items-center justify-center  fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <div className="w-16 h-16 border-4 border-x-secondaryDarker border-t-transparent border-solid rounded-full animate-spin"></div>
+    <div className="flex items-center justify-center my-3">
+      <div className="w-10 h-10 border-4 border-x-secondaryDarker border-t-transparent border-solid rounded-full animate-spin"></div>
     </div>
   );
 };
