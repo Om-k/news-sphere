1fcf9b8bc23d5895305c321cc64850e85d4b1fc8
Om-k <om.k.poojary@gmail.com>
2025-03-01 20:40:32 +0530

    Update: .env file

--- src/services/axiosInstances.ts
+++ src/services/axiosInstances.ts
@@ -1,25 +1,25 @@
 import axios from 'axios';
 
 export const guardianAPI = axios.create({
-    baseURL: '/api', 
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    params: {
-      'api-key': '74ae75ef-c662-4382-ae4e-da5b43782890',
-    },
-  });
+  baseURL: '/api', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  params: {
+    'api-key': import.meta.env.VITE_API_KEY_GAURDIAN_API,
+  },
+});
 
 export const newYorkTimesAPI = axios.create({
   baseURL: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
   params: {
-    'api-key': 'yLLbjfshrH5UGOsJQA8ZGxyLG6Rg5YC7',
+    'api-key': import.meta.env.VITE_API_KEY_NYT_API,
   },
 });
 
 export const newsAPI = axios.create({
   baseURL: 'https://newsapi.org/v2',
   headers: {
-    'Authorization': 'Bearer df97de98cd6b41619e490ea161a18cbe',
+    'Authorization': `Bearer ${import.meta.env.VITE_API_KEY_NEWS_API}`,
   },
 });
